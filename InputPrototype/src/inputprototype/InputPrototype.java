a3202e2dc705935c95166ecfe818cba3e584197a
RoyvandenHeuvel <0878963@hr.nl>
2015-10-08 13:03:43 +0200

    start

---
+++ InputPrototype/src/inputprototype/InputPrototype.java
@@ -0,0 +1,36 @@
+package inputprototype;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * @author Roy van den Heuvel
+ */
+public class InputPrototype {
+
+    public static void main(String[] args) {
+
+        String fileNameDefined = "..\\csv\\Monitoring.csv";
+
+        File file = new File(fileNameDefined);
+
+        try {
+
+            Scanner inputStream = new Scanner(file);
+
+            while (inputStream.hasNext()) {
+
+                String data = inputStream.next();
+                System.out.println(data);
+
+            }
+            inputStream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
