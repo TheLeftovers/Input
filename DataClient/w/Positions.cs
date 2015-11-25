eda1c09662357ca4b2c2ce710b1da54c26d10593
Johan Bos <bosbosjohan@gmail.com>
2015-11-25 18:44:09 +0100

    c# webservice console

---
+++ DataClient/w/Positions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace w
+{
+    public class Positions
+    {
+        public virtual string Time { get; set; }
+        public virtual long UnitId { get; set; }
+        public virtual DateTime Date { get; set; }
+        public virtual string RdY { get; set; }
+        public virtual int Speed { get; set; }
+        public virtual string Quality { get; set; }
+        public virtual string RdX { get; set; }
+        public virtual int Hdop { get; set; }
+        public virtual int Course { get; set; }
+        public virtual int NumSatellites { get; set; }
+
+        public Positions()
+        {}
+
+        public Positions(string Time, long UnitId, DateTime Date, string RdX, string RdY, int Speed, string Quality, int Hdop, int Course, int NumSatellites)
+        {
+            this.Course = Course;
+            this.Date = Date;
+            this.Hdop = Hdop;
+            this.NumSatellites = NumSatellites;
+            this.Quality = Quality;
+            this.RdX = RdX;
+            this.RdY = RdY;
+            this.Speed = Speed;
+            this.Time = Time;
+            this.UnitId = UnitId;
+        }
+    }
+}
---
+++ DataClient/w/Program.cs
@@ -0,0 +1,43 @@
+﻿using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cfg = new Configuration();
+            cfg.DataBaseIntegration(x =>
+            {
+                x.ConnectionString = "Server=localhost;database=project56;user id=postgres;password=root";
+                x.Driver<NHibernate.Driver.NpgsqlDriver>();
+                x.Dialect<NHibernate.Dialect.PostgreSQLDialect>();
+            });
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+            var sessionFactory = cfg.BuildSessionFactory();
+            using (var session = sessionFactory.OpenSession())
+            {
+                using (var tx = session.BeginTransaction())
+                {
+                    var positions = session.CreateCriteria<Positions>()
+                        .List<Positions>();
+                    foreach (var pos in positions)
+                    {
+                        Console.WriteLine("Units: " + pos.UnitId.ToString() + "\n" + "Speed:" + pos.Speed.ToString() + "\n");
+                    }
+                    tx.Commit();
+                }
+                Console.WriteLine("Press <ENTER> to exit...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
