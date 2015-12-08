82e0c10eae3ed632a999ca0e1edf020b74ffacfb
frunkyfrunk <verhagen.frank@gmail.com>
2015-12-08 15:59:21 +0100

    Login

--- DataClient/WebApplication/Account/Login.aspx.cs
+++ DataClient/WebApplication/Account/Login.aspx.cs
@@ -5,11 +5,21 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Owin;
 using WebApplication.Models;
+using System.Data.SqlClient;
+using System.Data;
+using NHibernate.Cfg;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Web.Security;
 
 namespace WebApplication.Account
 {
     public partial class Login : Page
     {
+        
+        public object Login1 { get; private set; }
+        public bool IsValidaded { get; private set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             RegisterHyperLink.NavigateUrl = "Register";
@@ -25,7 +35,66 @@ namespace WebApplication.Account
 
         protected void LogIn(object sender, EventArgs e)
         {
-            if (IsValid)
+
+
+            var cfg = new Configuration();
+            List<Users> ulist = new List<Users>();
+
+            cfg.DataBaseIntegration(x =>
+            {
+                x.ConnectionString = "Server=localhost;database=project56;user id=postgres;password=root";
+                x.Driver<NHibernate.Driver.NpgsqlDriver>();
+                x.Dialect<NHibernate.Dialect.PostgreSQLDialect>();
+            });
+
+
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddAssembly(typeof(Users).Assembly);
+            var sessionFactory = cfg.BuildSessionFactory();
+
+            using (var session = sessionFactory.OpenSession())
+            using (var tx = session.BeginTransaction())
+            {
+
+                var maillist = session.CreateSQLQuery("SELECT email FROM users").List();
+                var passwordlist = session.CreateSQLQuery("SELECT password FROM users").List();
+
+
+                for (int i=0; i < maillist.Count; i++)
+                {
+                    Users u = new Users(maillist[i].ToString(), passwordlist[i].ToString());
+                    ulist.Add(u);
+                }
+                tx.Commit();
+
+            }
+            
+
+            for (int i = 0; i < ulist.Count; i++)
+                {
+                    if (ulist[i].Email.Equals(Email.Text))
+                    {
+                        if (ulist[i].Password.Equals(Password.Text))
+                        {
+                            System.Diagnostics.Debug.WriteLine("Logged in as" + ulist[i].Email);
+                            MessageBox.Show(Page, "Logged in");
+                            IsValidaded = true;
+                            WebApplication.SiteMaster.LoggedIn = true;
+                            WebApplication.SiteMaster.UserName = ulist[i].Email;
+                        }
+                        else
+                        {
+                            MessageBox.Show(Page, "Invalid password");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show(Page, "This email is not registrated");
+                    }
+                }
+           
+
+            if (IsValidaded)
             {
                 // Validate the user password
                 var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
@@ -33,18 +102,20 @@ namespace WebApplication.Account
 
                 // This doen't count login failures towards account lockout
                 // To enable password failures to trigger lockout, change to shouldLockout: true
-                var result = signinManager.PasswordSignIn(Email.Text, Password.Text, RememberMe.Checked, shouldLockout: false);
+                var result = SignInStatus.Success;
 
                 switch (result)
                 {
                     case SignInStatus.Success:
                         IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+
+
                         break;
                     case SignInStatus.LockedOut:
                         Response.Redirect("/Account/Lockout");
                         break;
                     case SignInStatus.RequiresVerification:
-                        Response.Redirect(String.Format("/Account/TwoFactorAuthenticationSignIn?ReturnUrl={0}&RememberMe={1}", 
+                        Response.Redirect(String.Format("/Account/TwoFactorAuthenticationSignIn?ReturnUrl={0}&RememberMe={1}",
                                                         Request.QueryString["ReturnUrl"],
                                                         RememberMe.Checked),
                                           true);
@@ -56,6 +127,8 @@ namespace WebApplication.Account
                         break;
                 }
             }
+
+            }
+        
         }
     }
-}
---
+++ DataClient/WebApplication/MessageBox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace WebApplication
+{
+    public static class MessageBox
+    {
+        public static void Show(this Page Page, string Message)
+        {
+            Page.ClientScript.RegisterStartupScript(
+            Page.GetType(),
+            "MessageBox",
+            "<script lahguage='javascript'>alert ('" + Message + "');</script>");
+        }
+    }
+}
