cb075b7e54575006229d0ab5b6b52fb6eaf146eb
RoyvandenHeuvel <0878963@hr.nl>
2016-01-13 13:11:01 +0100

    parser lat lon conversion + maps page in dataclient

--- Parser/src/parser/Parser.java
+++ Parser/src/parser/Parser.java
@@ -24,6 +24,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Array;
 import static parser.FileWatcher.watchDirectoryPath;
 
 /**
@@ -400,8 +401,12 @@ public class Parser {
                 Time timeDate = Time.valueOf(time);
 
                 long longUnitId = Long.parseLong(unitId);
-                double floatRdx = Double.parseDouble(rdx);
-                double floatRdy = Double.parseDouble(rdy);
+                double doubleRdx = Double.parseDouble(rdx);
+                double doubleRdy = Double.parseDouble(rdy);
+                double[] coordinateArray = convertToLonLat(doubleRdx, doubleRdy);
+                double lat = coordinateArray[0];
+                double lon = coordinateArray[1];
+                
                 int intSpeed = Integer.parseInt(speed);
                 int intCourse = Integer.parseInt(course);
                 int intNumSatallites = Integer.parseInt(numSatallites);
@@ -413,8 +418,8 @@ public class Parser {
                 position.setHDOP(intHDOP);
                 position.setNumSatellites(intNumSatallites);
                 position.setQuality(quality);
-                position.setRdx(floatRdx);
-                position.setRdy(floatRdy);
+                position.setRdx(lat);
+                position.setRdy(lon);
                 position.setSpeed(intSpeed);
                 position.setTime(timeDate);
                 position.setUnitId(longUnitId);
@@ -450,5 +455,57 @@ public class Parser {
             returnValue = true;
         }
         return returnValue;
+    }
+    
+    private static double[] convertToLonLat(double rdx, double rdy){
+        int referenceRdX = 155000;
+        int referenceRdY = 463000;
+        
+        double dX = (double)(rdx - referenceRdX) * (double)(Math.pow(10,-5));
+        double dY = (double)(rdy - referenceRdY) * (double)(Math.pow(10,-5));
+
+        double sumN = 
+            (3235.65389 * dY) + 
+            (-32.58297 * Math.pow(dX, 2)) + 
+            (-0.2475 * Math.pow(dY, 2)) + 
+            (-0.84978 * Math.pow(dX, 2) * dY) + 
+            (-0.0655 * Math.pow(dY, 3)) + 
+            (-0.01709 * Math.pow(dX, 2) * Math.pow(dY, 2)) + 
+            (-0.00738 * dX) + 
+            (0.0053 * Math.pow(dX, 4)) + 
+            (-0.00039 * Math.pow(dX, 2) * Math.pow(dY, 3)) + 
+            (0.00033 * Math.pow(dX, 4) * dY) + 
+            (-0.00012 * dX * dY);
+        double sumE = 
+            (5260.52916 * dX) + 
+            (105.94684 * dX * dY) + 
+            (2.45656 * dX * Math.pow(dY, 2)) + 
+            (-0.81885 * Math.pow(dX, 3)) + 
+            (0.05594 * dX * Math.pow(dY, 3)) + 
+            (-0.05607 * Math.pow(dX, 3) * dY) + 
+            (0.01199 * dY) + 
+            (-0.00256 * Math.pow(dX, 3) * Math.pow(dY, 2)) + 
+            (0.00128 * dX * Math.pow(dY, 4)) + 
+            (0.00022 * Math.pow(dY, 2)) + 
+            (-0.00022 * Math.pow(dX, 2)) + 
+            (0.00026 * Math.pow(dX, 5));
+                // The city "Amsterfoort" is used as reference "WGS84" coordinate.
+            double referenceWgs84X = 52.15517;
+            double referenceWgs84Y = 5.387206;
+
+            double latitude = referenceWgs84X + (sumN / 3600);
+            double longitude = referenceWgs84Y + (sumE / 3600);
+
+            // Input
+            // x = 122202
+            // y = 487250
+            //
+            // Result
+            // "52.372143838117, 4.90559760435224"
+            double[] result = new double[2];
+            result[0] = latitude;
+            result[1] = longitude;
+            
+            return result;
     }
 }
